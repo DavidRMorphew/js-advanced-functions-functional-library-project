8700dc5f227dba92d8205dbae141b16a7655f8cf
DavidRMorphew <morphewd@umich.edu>
2021-04-10 14:58:41 -0400

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callBackFunction) {
+      if (!!collection.length){
+        for (const element of collection){ 
+          callBackFunction(element);
+        }
+      } else {
+        for (const property in collection){
+          callBackFunction(collection[property]);
+        }
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callBackFunction) {
+      const newArray = [];
+      if (!!collection.length){
+        for (const element of collection){ 
+          newArray.push(callBackFunction(element));
+        }
+      } else {
+        for (const property in collection){
+          newArray.push(callBackFunction(collection[property]));
+        }
+      }
+      return newArray;
     },
 
     reduce: function() {
